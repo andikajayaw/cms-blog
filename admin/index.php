3a45be384c6ba33925dd812ac27f598ab7f27215
andikajayaw <andikajayawiguna@gmail.com>
2021-01-19 20:43:04 +0800

    adding widgets

--- admin/index.php
+++ admin/index.php
@@ -20,6 +20,132 @@
                     </div>
                 </div>
                 <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                            $query = "SELECT * FROM posts";
+                                            $stmt = mysqli_query($connection, $query);
+                                            $total_post = mysqli_num_rows($stmt);
+                                            // confirm($stmt);
+                                            // while($row = mysqli_fetch_assoc($stmt)) {
+                                            //     $total_post = $row['total_posts'];
+                                            // }
+                                        ?>
+                                        <div class='huge'><?php echo $total_post; ?></div>
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="posts.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                            $query = "SELECT * FROM comments";
+                                            $stmt = mysqli_query($connection, $query);
+                                            $total_comments = mysqli_num_rows($stmt);
+                                            // confirm($stmt);
+                                            // while($row = mysqli_fetch_assoc($stmt)) {
+                                            //     $total_comments = $row['total_comments'];
+                                            // }
+                                        ?>
+                                        <div class='huge'><?php echo $total_comments; ?></div>
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                            $query = "SELECT * FROM users";
+                                            $stmt = mysqli_query($connection, $query);
+                                            $total_users = mysqli_num_rows($stmt);
+                                            // confirm($stmt);
+                                            // while($row = mysqli_fetch_assoc($stmt)) {
+                                            //     $total_comments = $row['total_comments'];
+                                            // }
+                                        ?>
+                                        <div class='huge'><?php echo $total_users; ?></div>
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                            $query = "SELECT * FROM categories";
+                                            $stmt = mysqli_query($connection, $query);
+                                            $total_categories = mysqli_num_rows($stmt);
+                                            // confirm($stmt);
+                                            // while($row = mysqli_fetch_assoc($stmt)) {
+                                            //     $total_comments = $row['total_comments'];
+                                            // }
+                                        ?>
+                                        <div class='huge'><?php echo $total_categories ?></div>
+                                        <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="categories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
             </div>
             <!-- /.container-fluid -->
