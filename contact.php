0f531d3fc43a69dfd27ce579c8035add55bb026a
andikajayaw <andikajayawiguna@gmail.com>
2021-01-27 22:00:49 +0800

    update contact page

--- contact.php
+++ contact.php
@@ -2,47 +2,10 @@
 <?php  include "includes/header.php"; ?>
 <?php  
     if(isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        if(!empty($username) && !empty($email) && !empty($password)){
-            $username = mysqli_real_escape_string($connection, $username);
-            $email = mysqli_real_escape_string($connection, $email);
-            $password = mysqli_real_escape_string($connection, $password);
-
-            $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+        $to = "andikaja@andikajayaw.com";
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
     
-            // $query = "SELECT rand_salt FROM users";
-            // $stmt = mysqli_query($connection, $query);
-    
-            // if(!$stmt) {
-            //     die("QUERY FAILED ".mysqli_error($connection));
-            // }
-    
-            // $row = mysqli_fetch_assoc($stmt);
-            // $randSalt = $row['rand_salt'];
-
-            // $password = crypt($password, $randSalt);
-    
-            $queryRegister = "INSERT INTO users(username, email, password, roles) 
-            VALUES('$username', '$email', '$password', 'subscriber')";
-            $stmtRegister = mysqli_query($connection, $queryRegister);
-            if(!$stmtRegister) {
-                die("QUERY FAILED ".mysqli_error($connection));
-            }
-            $message = "New user has been registered!";
-            // echo "<p class='bg-success'>New user has been registered!</p>";
-        } else {
-            $message = "Field cannot be empty!";
-        }
-
-        // while($row = mysqli_fetch_assoc($stmt)){
-        //     $randSalt = $row['rand_salt'];
-        //     echo $randSalt;
-        // }
-    
-    } else {
-        $message = '';
     }
 ?>
 
@@ -72,7 +35,7 @@
                             <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your subject">
                         </div>
                         <div class="form-group">
-                            <textarea type="text" name="subject" id="subject" class="form-control" cols="50" rows="10"></textarea>
+                            <textarea type="text" name="body" id="body" class="form-control" cols="50" rows="10"></textarea>
                         </div>
                         <!-- <div class="form-group">
                             <label for="password" class="sr-only">Password</label>
