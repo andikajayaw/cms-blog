eac6d017c012f8f026a1436dcdbdb95b831cefca
andikajayaw <andikajayawiguna@gmail.com>
2021-02-05 21:41:23 +0800

    reset password using mailtrap

---
+++ classes/config.php
@@ -0,0 +1,9 @@
+<?php 
+    class Config {
+        const SMTP_HOST     = 'smtp.mailtrap.io';
+        const SMTP_PORT     =  2525;
+        const SMTP_USER     = '78922163324fc6';
+        const SMTP_PASSWORD = '220709cf0696f9';
+    }
+
+?>
--- forgot.php
+++ forgot.php
@@ -1,5 +1,17 @@
-<?php include "includes/db.php"; ?>
-<?php include "includes/header.php"; ?>
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    include "includes/db.php"; 
+    include "includes/header.php"; 
+ 
+    require './vendor/autoload.php';
+    // require './vendor/phpmailer/phpmailer/PHPMailer.php';
+    require './classes/config.php';
+
+?>
 
 <?php 
 
@@ -19,6 +31,34 @@
                 confirm($stmt);
                 mysqli_stmt_bind_param($stmt, "s", $email);
                 mysqli_stmt_execute($stmt);
+
+                /** 
+                 * CONFIGURE PHP MAILER
+                */
+                
+                $mail = new PHPMailer(true);
+
+                try {
+                    // $mail->SMTPDebug =  SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+                    $mail->isSMTP();                                          // Send using SMTP
+                    $mail->Host       = Config::SMTP_HOST;                    // Set the SMTP server to send through
+                    $mail->Username   = Config::SMTP_USER;                    // SMTP username
+                    $mail->Password   = Config::SMTP_PASSWORD;                    // SMTP password
+                    $mail->Port       = Config::SMTP_PORT;                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;       // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                    $mail->SMTPAuth   = true;                                 // Enable SMTP authentication
+                    
+                    $mail->isHTML(true);
+    
+                    $mail->setFrom('admin2@mail.com', 'Andika Jaya');
+                    $mail->addAddress($email);
+                    $mail->Subject = 'This is a test email'; 
+                    $mail->Body = 'Email Body';
+                    $mail->send();
+                    echo "Email sent!";
+                } catch(Exception $e) {
+                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                }
             }
         }
     }
